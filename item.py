74ed52e353cd9ae18b0374ac576d21ce038864cb
Rasmus Ansin <rasmus.ansin@gmail.com>
2015-05-07 15:09:49 +0200

    added some new items

--- item.py
+++ item.py
@@ -3,16 +3,27 @@ EDIBLE = 1
 MAGICAL = 2
 TASTY = 4
 PETTABLE = 8
-PLAYFUL = 16
+PLAYABLE = 16
+POISONOUS = 32
+HEALING = 64
 
 #each value in the items dictionary corresponds to a property of the item
 items = {
     "banana": EDIBLE | PETTABLE,
     "orange": EDIBLE,
     "child": EDIBLE | TASTY,
-    "tire": PLAYFUL,
-    
+    "tire": PLAYABLE,
+    "rotten apple": EDIBLE | POISONOUS,
+    "rusty nail": PLAYABLE | POISONOUS,
+    "medicine": EDIBLE | HEALING,
+    "golden apple": EDIBLE | HEALING,
+    "book": PLAYABLE,
 }
+
+def hasProperty(itemName, prop):
+    if itemName not in items:
+        return False
+    return item[itemName] & prop
 
 def isEdible(itemName):
     if itemName not in items: return False
