a644741cd01c93de7f54140b1b97f92f5ec0a5b5
Simon Forssell <simonfacoin@gmail.com>
2015-05-25 00:33:27 +0200

    Small fix in shop.py.

--- shop.py
+++ shop.py
@@ -9,26 +9,26 @@ class Shop:
     def __init__(self):
         self.itemAndCostList = []
         self._populate()
-        
+
     def _populate(self):
         for _ in range(8): #start off with a few items...
-            self.addRandomItem()
-            
+            self._addRandomItem()
+
     def getItemsJSON(self):
         json.dumps({"error": False, "items": self.itemAndCostList})
-            
+
     def update(self):
         self._removeRandomItem()
         self._addRandomItem()
-        
+
     def _removeRandomItem(self):
         if self.itemAndCostList: #don't pop if it's empty
             self.itemAndCostList.pop(random.randrange(len(self.itemAndCostList)))
-        
+
     def _addRandomItem(self):
         itemStr = random.choice(item.items.keys())
         cost = random.randint(4, 8)
         if item.isExpensive(itemStr):
             cost += 8
-        
+
         self.itemAndCostList.append([itemStr, cost])
